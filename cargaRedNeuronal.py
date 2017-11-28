a71d4265d2ccd9ca55b36e429fb69004ee051f3d
jeixonx <triana.jeison@gmail.com>
2017-11-27 21:36:13 -0500

    red neuronal convulsional, entrenamiengo, guardado y carga

---
+++ cargaRedNeuronal.py
@@ -0,0 +1,41 @@
+# Larger CNN for the MNIST Dataset
+import numpy
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras.models import model_from_json
+from keras import backend as K
+K.set_image_dim_ordering('th')
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# reshape to be [samples][pixels][width][height]
+X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+scores = loaded_model.evaluate(X_test, y_test, verbose=0)
+print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
